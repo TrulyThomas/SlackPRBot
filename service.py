6ab1be8ddf08e196e9cb3f86e7a6443069e8ba7a
Molitany <thor@beregaard.dk>
2021-10-01 08:30:51 +0200

    pr

--- service.py
+++ service.py
@@ -8,26 +8,33 @@ def PopulateUserlist(client, userList):
     random.shuffle(userList)
 
 def SaveUsersToFile(userList):
-    with open('developers.txt', 'w') as outfile:
+    with open('dev-stack.txt', 'w') as outfile:
         outfile.write('[')
         for user in userList[:-1]:
             outfile.write(f"'{user}',")
-        outfile.write(f"'{userList[len(userList) - 1]}']")
+        if (len(userList) != 0):
+            outfile.write(f"'{userList[len(userList) - 1]}']")
+        else:
+            outfile.write("]")
 
-def PRAssignedToUsers(userList):
-    if(len(userList) > 2):
+def PRAssignedToUsers(client, userList):
+    if(len(userList) > 1):
         user0 = userList.pop()
         user1 = userList.pop()
     elif(len(userList) == 1):
         user0 = userList.pop()
-        PopulateUserlist()
+        PopulateUserlist(client, userList)
         userList.remove(user0)
         user1 = userList.pop()
         userList.append(user0)
-    
+    else:
+        PopulateUserlist(client, userList)
+        user0 = userList.pop()
+        user1 = userList.pop()
+
     SaveUsersToFile(userList)
     return [user0, user1]
 
 def GetUsersFromFile():
-    with open('developers.txt', 'r') as outfile:
+    with open('dev-stack.txt', 'r') as outfile:
         return outfile.read()
