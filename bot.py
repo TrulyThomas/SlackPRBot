0fc529a97c8ffd2c8dd589f6acc879356853ebdb
Molitany <thor@beregaard.dk>
2021-10-07 12:21:18 +0200

    pr

--- bot.py
+++ bot.py
@@ -1,87 +1,22 @@
-import slack
-import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
-import json
-import random
+from flask import Flask, request
+import service
 
+userList = []
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(
-    os.environ['SIGNING_SECRET'], '/slack/events', app)
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-def GetUsersFromFile():
-    with open('developers.txt', 'r') as outfile:
-        return outfile.read()
-        
-fileData = eval(GetUsersFromFile())
-if(len(fileData) == 0):
-    userList = []
-else:
-    userList = fileData
+service.AuthorizeGithubInstallation()
+userList = service.GetUsersFromFile(userList)
 
-def SaveUsersToFile():
-    result = client.users_list()
-    for member in result['members']:
-        if(member['is_bot'] == False and member['deleted'] == False and member['real_name'] != 'Slackbot'):
-            userList.append([member['id'], member['real_name']])
-    
-    random.shuffle(userList)
-
-    with open('developers.txt', 'w') as outfile:
-        outfile.write('[')
-        for user in userList[:-1]:
-            outfile.write(f"{user},")
-        outfile.write(f'{userList[len(userList) - 1]}]')
-
-
-def SaveCurrentUsersToFile():
-    if(len(userList) == 0):
-        SaveUsersToFile()
-    with open('developers.txt', 'w') as outfile:
-        outfile.write('[')
-        for user in userList[:-1]:
-            outfile.write(f"{user},")
-        outfile.write(f'{userList[len(userList) - 1]}]')
-
-
-def AddUser(user_id, real_name):
-    data['users'].append({
-        'slack_id': user_id,
-        'real_name': real_name
-    })
-
-
-def PRAssignedToUsers():
-    if(len(userList) > 2):
-        user0 = userList.pop()
-        user1 = userList.pop()
-    elif(len(userList) == 1):
-        user0 = userList.pop()
-        SaveUsersToFile()
-        userList.remove(user0)
-        user1 = userList.pop()
-        userList.append(user0)
-    else:
-        SaveUsersToFile()
-    SaveCurrentUsersToFile()
-    return [user0, user1]
-
-if(len(userList) == 0):
-    SaveUsersToFile()
-
-@slack_events_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    text = event.get('text')
-    if "Pull request opened by" in text:
-        assignees = PRAssignedToUsers()
-        client.chat_postMessage(
-            channel="#pr", text=f"<@{assignees[0][0]}> and <@{assignees[1][0]}> should review")
+@app.route('/github/events', methods=['POST'])
+def GithubEvent():
+    action = request.json.get('action')
+    if (action == 'opened' or action == 'reopened'):
+        service.AssignReviewers(request.json.get('pull_request'), userList)
+    return "return"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- service.py
+++ service.py
@@ -1,40 +1,119 @@
 import random
+from sys import getdefaultencoding
+from jwt import (
+    JWT,
+    jwk_from_pem
+)
+from jwt.utils import get_int_from_datetime
+from datetime import datetime, timedelta, timezone
+import os
+import requests
+import json
+import shutil
 
-def PopulateUserlist(client, userList):
-    result = client.users_list()
-    for member in result['members']:
-        if(member['is_bot'] == False and member['deleted'] == False and member['real_name'] != 'Slackbot'):
-            userList.append(member['id'])
-    random.shuffle(userList)
 
 def SaveUsersToFile(userList):
-    with open('dev-stack.txt', 'w') as outfile:
-        outfile.write('[')
-        for user in userList[:-1]:
-            outfile.write(f"'{user}',")
-        if (len(userList) != 0):
-            outfile.write(f"'{userList[len(userList) - 1]}']")
-        else:
-            outfile.write("]")
+    AuthorizeGithubInstallation()
+    token = os.environ.get('INSTALL_TOKEN')
+    response = requests.get("https://api.github.com/orgs/aau-giraf/teams/2021E/members", headers={
+        'Authorization': f'Bearer {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }).json()
+    
 
-def PRAssignedToUsers(client, userList):
+    shutil.copy('developers.json', 'dev_stack.json')
+    userList = json.loads(open('dev_stack.json', 'r').read())
+    return userList
+
+
+def PRAssignedToUsers(userList, PROwnerName):
     if(len(userList) > 1):
-        user0 = userList.pop()
-        user1 = userList.pop()
+        user0, user0ID = GetDeveloper(userList, PROwnerName)
+        user1, user1ID = GetDeveloper(userList, PROwnerName)
     elif(len(userList) == 1):
-        user0 = userList.pop()
-        PopulateUserlist(client, userList)
-        userList.remove(user0)
-        user1 = userList.pop()
-        userList.append(user0)
+        if (list(userList.values())[0] == PROwnerName):
+            userList = SaveUsersToFile(userList)
+            user0, user0ID = GetDeveloper(userList, PROwnerName)
+            user1, user1ID = GetDeveloper(userList, PROwnerName)
+        else: 
+            user0, user0ID = GetDeveloper(userList, PROwnerName)
+            userList = SaveUsersToFile(userList)
+            userList.pop(user0ID)
+            user1, user1ID = GetDeveloper(userList, PROwnerName)
     else:
-        PopulateUserlist(client, userList)
-        user0 = userList.pop()
-        user1 = userList.pop()
+        userList = SaveUsersToFile(userList)
+        user0, user0ID = GetDeveloper(userList, PROwnerName)
+        user1, user1ID = GetDeveloper(userList, PROwnerName)
 
-    SaveUsersToFile(userList)
+    with open('dev_stack.json', 'w') as file:
+        file.write(json.dumps(userList))
+
     return [user0, user1]
 
-def GetUsersFromFile():
-    with open('dev-stack.txt', 'r') as outfile:
-        return outfile.read()
+
+def GetUsersFromFile(userList):
+    with open('dev_stack.json', 'r') as outfile:
+        text = outfile.read()
+        if (text == ''):
+            outfile.close()
+            return SaveUsersToFile(userList)
+        else:
+            return json.loads(text)
+
+
+def GetGithubInstallations():
+    with open('Github_private_key.pem', 'rb') as pem:
+        privateKey = jwk_from_pem(pem.read())
+
+    ghToken = JWT().encode({
+        'iat': get_int_from_datetime(datetime.now(timezone.utc)),
+        'exp': get_int_from_datetime(datetime.now(timezone.utc) + timedelta(minutes=5)),
+        'iss': os.environ.get('GIT_APP_ID')
+    }, privateKey, alg='RS256')
+    os.environ['JWT'] = ghToken
+
+    response = requests.get("https://api.github.com/app/installations", headers={
+        'Authorization': f'Bearer {ghToken}',
+        'Accept': 'application/vnd.github.v3+json'
+    }).json()
+
+    return response
+
+
+def AuthorizeGithubInstallation():
+    JWT = os.environ.get('JWT')
+    installations = GetGithubInstallations()
+    response = requests.post(installations[0].get('access_tokens_url'), headers={
+        'Authorization': f'Bearer {JWT}',
+        'Accept': 'application/vnd.github.v3+json'
+    }).json()
+    if (response.get('token') is not None):
+        os.environ['INSTALL_TOKEN'] = response.get('token')
+
+
+def AssignReviewers(pullRequest, userList):
+    PRUrl = pullRequest.get('url')
+    assignees = PRAssignedToUsers(userList, pullRequest.get('user').get('login'))
+    AuthorizeGithubInstallation()
+    response = PostReviewers(PRUrl, assignees)
+    print(response)
+
+
+def PostReviewers(PRUrl, assignees):
+    INSTALL_TOKEN = os.environ.get('INSTALL_TOKEN')
+    json = {
+        'reviewers': assignees
+    }
+    return requests.post(f'{PRUrl}/requested_reviewers', headers={
+        'Authorization': f'Bearer {INSTALL_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json'
+    }, json=json)
+
+def GetDeveloper(userList, name):
+    DevID,DevName = random.choice(list(userList.items()))
+    if (DevName == name):
+        return GetDeveloper(userList, DevName)
+    else:
+        userList.pop(DevID)
+        return DevName, DevID
+        
